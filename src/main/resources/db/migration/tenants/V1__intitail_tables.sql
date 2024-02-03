aea54b1e186f3cfdbc75032fa96187c7d8af8922
Thivyathivya <thivya.kraysinfotech@gmail.com>
2024-02-03 15:59:53 +0530

    UUID chages updated by thivya

--- src/main/resources/db/migration/tenants/V1__intitail_tables.sql
+++ src/main/resources/db/migration/tenants/V1__intitail_tables.sql
@@ -17,85 +17,87 @@ CREATE TABLE IF NOT EXISTS user_details
 );
 
 -- Table: public.course
-CREATE TABLE IF NOT EXISTS course
+CREATE TABLE IF NOT EXISTS public.course
 (
-    course_id integer NOT NULL,
-    author_name character varying(255) ,
-    category character varying(255) ,
-    date timestamp(6) without time zone,
-    description character varying(255) ,
-    enrolled bigint,
-    language character varying(255) ,
-    overview character varying(255) ,
     price integer,
     ratings integer,
+    date timestamp(6) without time zone,
+    enrolled bigint,
+    user_id bigint NOT NULL,
+    course_id uuid NOT NULL,
+    author_name character varying(255) ,
+    category character varying(255) ,
+    description text  ,
+    language character varying(255) ,
+    overview character varying(255) ,
     thumb_nail text ,
     title character varying(255) ,
     what_you_will_learn text ,
     CONSTRAINT course_pkey PRIMARY KEY (course_id)
 );
 -- Table: public.section
-CREATE TABLE IF NOT EXISTS section
+CREATE TABLE IF NOT EXISTS public.section
 (
-    section_id integer NOT NULL,
     key integer,
+    course_id uuid,
+    section_id uuid NOT NULL,
     title character varying(255) ,
-    course_id integer,
     CONSTRAINT section_pkey PRIMARY KEY (section_id),
     CONSTRAINT fkoy8uc0ftpivwopwf5ptwdtar0 FOREIGN KEY (course_id)
-        REFERENCES course (course_id) MATCH SIMPLE
+        REFERENCES public.course (course_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 -- Table: public.sub_section
-CREATE TABLE IF NOT EXISTS sub_section
+CREATE TABLE IF NOT EXISTS public.sub_section
 (
-    sub_section_id integer NOT NULL,
-    description character varying(255) ,
     key integer,
-    link character varying(255) ,
+    section_id uuid,
+    sub_section_id uuid NOT NULL,
+    description text ,
+    link text ,
     title character varying(255) ,
-    section_id integer,
     CONSTRAINT sub_section_pkey PRIMARY KEY (sub_section_id),
     CONSTRAINT fk6qmlkrayugejciehhvnyoohut FOREIGN KEY (section_id)
-        REFERENCES section (section_id) MATCH SIMPLE
+        REFERENCES public.section (section_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
 -- Table: public.quiz
-CREATE TABLE IF NOT EXISTS quiz
+CREATE TABLE IF NOT EXISTS public.quiz
 (
-    quiz_id integer NOT NULL,
     answer integer NOT NULL,
     key integer,
+    quiz_id uuid NOT NULL,
+    sub_section_id uuid,
+    question text ,
+    title character varying(255) ,
     options character varying(255)[] ,
-    question character varying(255) ,
-    sub_section_id integer,
-    title character varying(255)  ,
     CONSTRAINT quiz_pkey PRIMARY KEY (quiz_id),
     CONSTRAINT fkbq87nt5wprr78k7d6mqwrenjf FOREIGN KEY (sub_section_id)
-        REFERENCES sub_section (sub_section_id) MATCH SIMPLE
+        REFERENCES public.sub_section (sub_section_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 -- Table: public.quiz_rank
-CREATE TABLE IF NOT EXISTS quiz_rank
+CREATE TABLE IF NOT EXISTS public.quiz_rank
 (
-    rank_id integer NOT NULL,
     badge integer NOT NULL,
     energy_points integer,
-    sub_section_id integer,
     user_id bigint,
+    rank_id uuid NOT NULL,
+    sub_section_id uuid,
     CONSTRAINT quiz_rank_pkey PRIMARY KEY (rank_id)
-);
+)
+;
 -- Table: public.cart
-CREATE TABLE IF NOT EXISTS cart
+CREATE TABLE IF NOT EXISTS public.cart
 (
-    cart_id bigint NOT NULL,
-    course_id integer,
     create_date timestamp(6) without time zone,
     user_id bigint,
+    cart_id uuid NOT NULL,
+    course_id uuid,
     CONSTRAINT cart_pkey PRIMARY KEY (cart_id)
 );
 
