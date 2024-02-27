a38ab8bdbd5eaf1226ed2a62c2209dd54e638417
Thivyathivya <thivya.kraysinfotech@gmail.com>
2024-02-27 17:23:39 +0530

    Revert "add exaple in swagger updated by thivya"
    This reverts commit aad2c5a2ba11b1615132e62294522f9ef2658007.

--- src/main/java/com/LMS/userManagement/dto/ProfileDto.java
+++ src/main/java/com/LMS/userManagement/dto/ProfileDto.java
@@ -1,6 +1,5 @@
 package com.LMS.userManagement.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,18 +8,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileDto {
-    @Schema(description = "userId",example = "1")
-    public Long id;
-    @Schema(description = "name",example = "Ram")
-    public String name;
-    @Schema(description = "gender",example = "male")
+    private Long id;
+    private String name;
+
     public String gender;
-    @Schema(description = "school",example = "LMS school")
+
     public String school;
-    @Schema(description = "standard",example = "10")
+
     public Integer standard;
-    @Schema(description = "city",example = "Chennai")
+
     public String city;
-    @Schema(description = "country",example = "India")
+
     public String country;
 }
