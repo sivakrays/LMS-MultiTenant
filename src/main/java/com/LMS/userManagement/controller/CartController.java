aad2c5a2ba11b1615132e62294522f9ef2658007
Thivyathivya <thivya.kraysinfotech@gmail.com>
2024-02-27 17:14:10 +0530

    add exaple in swagger updated by thivya

--- src/main/java/com/LMS/userManagement/controller/CartController.java
+++ src/main/java/com/LMS/userManagement/controller/CartController.java
@@ -1,7 +1,14 @@
 package com.LMS.userManagement.controller;
 
+import com.LMS.userManagement.swaggerResponse.CartDetailResponse;
+import com.LMS.userManagement.swaggerResponse.ErrorResponse;
 import com.LMS.userManagement.model.Cart;
 import com.LMS.userManagement.service.CartService;
+import com.LMS.userManagement.swaggerResponse.SaveCartResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,15 +27,33 @@ public class CartController {
     private CartService cartService;
 
     @PostMapping("/saveCart")
+    @Operation(
+            summary = "Save Cart",
+            description = "Add courses in the cart",
+            responses = {@ApiResponse(description = "Success",responseCode = "200",content = @Content(mediaType = "application/json", schema = @Schema(implementation = SaveCartResponse.class))),
+                    @ApiResponse(description = "forbidden", responseCode = "403", content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErrorResponse.class))),
+            })
     public ResponseEntity<?> saveCart(@RequestBody Cart cart){
         return cartService.saveCart(cart);
     }
 
     @GetMapping("/getCartByUserId")
+    @Operation(
+            summary = "getCartByUserId",
+            description = "get courses in the cart",
+            responses = {@ApiResponse(description = "Success",responseCode = "200",content = @Content(mediaType = "application/json", schema = @Schema(implementation = CartDetailResponse.class))),
+                    @ApiResponse(description = "forbidden", responseCode = "403", content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErrorResponse.class))),
+            })
     public ResponseEntity<?> getCartDetailByUserId(@RequestParam Long userId){
         return cartService.getCartDetailByUserId(userId);
     }
     @DeleteMapping("/deleteCartById")
+    @Operation(
+            summary = "deleteCartById",
+            description = "delete courses in the cart",
+            responses = {@ApiResponse(description = "Success",responseCode = "200",content = @Content(mediaType = "application/json", schema = @Schema(implementation = CartDetailResponse.class))),
+                    @ApiResponse(description = "forbidden", responseCode = "403", content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErrorResponse.class))),
+            })
     public ResponseEntity<?> deleteCartById(@RequestParam UUID cartId){
         return cartService.deleteCartById(cartId);
     }
--- src/main/java/com/LMS/userManagement/dto/ProfileDto.java
+++ src/main/java/com/LMS/userManagement/dto/ProfileDto.java
@@ -1,5 +1,6 @@
 package com.LMS.userManagement.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,16 +9,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileDto {
-    private Long id;
-    private String name;
-
+    @Schema(description = "userId",example = "1")
+    public Long id;
+    @Schema(description = "name",example = "Ram")
+    public String name;
+    @Schema(description = "gender",example = "male")
     public String gender;
-
+    @Schema(description = "school",example = "LMS school")
     public String school;
-
+    @Schema(description = "standard",example = "10")
     public Integer standard;
-
+    @Schema(description = "city",example = "Chennai")
     public String city;
-
+    @Schema(description = "country",example = "India")
     public String country;
 }
