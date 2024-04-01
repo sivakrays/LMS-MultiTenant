e8b66061f6ce383923763c0001dc1bdde418268c
Thivyathivya <thivya.kraysinfotech@gmail.com>
2024-04-01 12:59:45 +0530

    change in security config for cors issue updated by thivya

--- src/main/java/com/LMS/userManagement/securityConfig/SecurityConfiguration.java
+++ src/main/java/com/LMS/userManagement/securityConfig/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,7 +46,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-       http.cors(AbstractHttpConfigurer::disable)
+       http.cors(Customizer.withDefaults())
                .csrf(AbstractHttpConfigurer::disable)
                         .authorizeHttpRequests(auth->
                             auth   .requestMatchers("/lms/api/auth/**").permitAll()
