0772f57b74902702486d2d188eeb8ce288d8dce4
Thivyathivya <thivya.kraysinfotech@gmail.com>
2024-04-01 15:37:59 +0530

    Course Details modified updated by thivya

--- src/main/java/com/LMS/userManagement/dto/CourseDetailDto.java
+++ src/main/java/com/LMS/userManagement/dto/CourseDetailDto.java
@@ -1,5 +1,6 @@
 package com.LMS.userManagement.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 
 import java.util.Date;
@@ -7,15 +8,26 @@ import java.util.UUID;
 
 public interface CourseDetailDto {
 
+    @JsonProperty("courseId")
     public UUID getCourse_id();
-    public Long gteUser_id();
+    @JsonProperty("userId")
+    public Long getUser_id();
+    @JsonProperty("title")
     public String getTitle();
+    @JsonProperty("authorName")
     public String getAuthor_name();
+    @JsonProperty("thumbNail")
     public String getThumb_nail();
+    @JsonProperty("category")
     public String getCategory();
+    @JsonProperty("ratings")
     public Integer getRatings();
+    @JsonProperty("language")
     public String getLanguage();
+    @JsonProperty("price")
     public Integer getPrice();
+    @JsonProperty("date")
     public Date getDate();
+    @JsonProperty("IsHtmlCourse")
     public Boolean getIs_html_course();
 }
