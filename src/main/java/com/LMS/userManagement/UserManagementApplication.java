f3bd434d3318312cc90c346e5aaf2886b77c5025
sivakrays <siva.krays@gmail.com>
2024-04-03 11:34:38 +0530

    Added config , security configs

--- src/main/java/com/LMS/userManagement/UserManagementApplication.java
+++ src/main/java/com/LMS/userManagement/UserManagementApplication.java
@@ -3,13 +3,13 @@ package com.LMS.userManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class UserManagementApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
-
 
 	}
 
